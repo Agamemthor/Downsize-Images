ccd2f398d71a48eaeb8ca36a1df99ebf982dcf8a
Agamemthor <71598557+Agamemthor@users.noreply.github.com>
2022-07-29 17:27:41 +0200

    Create main.py
    Main script to adjust image size. Suggest running a test to see if its to your liking before running it.

---
+++ main.py
@@ -0,0 +1,83 @@
+#Script to loop through a given folder and all subfolders
+#Intention is to save space from large image files. Most of the time I don't care about giant images.
+#Downsize by saving from .bmp to jpg, or saving with a smaller resolution
+#Sometimes the new file is actually larger. Don't understand why, I just included a check to see if we actually save space
+#Modify starting parameters, and make sure the temp folder exists.
+
+import cv2
+import os
+import shutil
+import defsImageManipulation as dim #early attempt to have a library of image manipulations
+import time
+
+maxWidth = 1600
+maxHeight = 1600
+maxMargin = 20 #incase maxWidth or Height is 1608 or something, some margin applied to prevent unnecessary adjustments 
+path = r"C:\myfolderwithimages"
+tmpFolder = r"C:\tmp\resize"
+
+tic = time.perf_counter()
+for root, dirs, files in os.walk(path):    
+    for file in files: 
+        try:
+            filePath = os.path.join(root, file)
+        except:
+            print('The path is invalid: ' + path)
+            continue
+              
+        bChange = False        
+
+        #get current extension and name
+        split = file.split('.')
+        name = split[0]
+        ext = split[1].lower()
+
+        #convert to jpg if bmp
+        #probably better off to include this section in bChange later on, and only save the image once instead of twice.
+        if ext == 'bmp':
+            filePath = dim.ConvertToJPG(filePath)
+            ext = 'jpg'
+
+        #load image and get parameters
+        try:
+            img = cv2.imread(filePath)            
+            width = int(img.shape[1])
+            height = int(img.shape[0])
+        except:
+            print('The file is not a valid image: ' + filePath)
+            continue
+
+        #check if img is landscape or portrait, and limit height and width
+        #apply some margin to avoid resizing only slighty and in some cases increase filesize
+        if width > height:
+            if width > (maxWidth + maxMargin):
+                factor = width/maxWidth
+                newWidth=maxWidth
+                newHeight = int(height / factor)
+                bChange = True
+        else:
+            if height > (maxHeight + maxMargin):
+                factor =  height/maxHeight
+                newHeight=maxHeight
+                newWidth = int(width / factor)
+                bChange = True
+
+        #if image exceeded max bounds, rescale it and save in temp directly. 
+        #if resized image is smaller than the original file, copy it over
+        if bChange:
+            curSize = os.path.getsize(filePath)
+
+            tmpPath = os.path.join(tmpFolder, name + '.' + ext)
+            scaledImg = cv2.resize(img, (newWidth, newHeight), interpolation=cv2.INTER_AREA)
+
+            cv2.imwrite(tmpPath, scaledImg)
+
+            if os.path.getsize(tmpPath) < curSize:
+                cv2.imwrite(filePath, scaledImg)
+
+            os.remove(tmpPath)
+                
+
+toc = time.perf_counter()
+
+print(f"Finished {toc - tic:0.4f} seconds")
